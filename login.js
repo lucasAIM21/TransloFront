94441c23010e5ca91e6769d0a11d4fd977d74a59
Lucas Ingaruca <lucaspe123321@gmail.com>
2026-04-21 16:01:25 -0500

    aea

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-const btn = document.getElementById("btn1");
+const btn = document.getElementById("btnLogin");
 
 btn.addEventListener("click", () => {
     window.location.href = "views/Principal.html";
